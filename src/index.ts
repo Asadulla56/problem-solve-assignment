797f9cc906847ea1c37b0aca3cec1b1fbe8820bc
Asadulla56 <mdasadullahoque294@gmail.com>
2025-05-08 16:15:57 +0600

    set up project

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+//problem 1
+
+function formatString(input: string, toUpper: boolean = true): string {
+    return toUpper ? input.toUpperCase() : input.toLowerCase();
+  }
+  console.log(formatString("Hello"));
+  
+ 
